c1a6e63bdf15c6fc90499c70d5e78e8bd0bd11d3
atesin <atesin@users.noreply.github.com>
2020-07-04 20:07:30 -0400

    Update httpy.py

--- httpy.py
+++ httpy.py
@@ -311,8 +311,7 @@ Requires Ncat to function online (https://nmap.org/ncat)
 Default configuration file: httpy.conf
 
 httpy just takes HTTP requests from standard input and writes responses to
-standard output and logs to standard error, it requires external program "ncat"
-to function online
+standard output and logs to standard error
 
 httpy is licensed under WTFPL and published without any warranty, i wrote it
 just to learn python in a fun way so it may contain errors, now you are adviced
